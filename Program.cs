3cd29db227b867775102b1b8bed879ab44061a9d
okuoku <mjt@cltn.org>
2023-02-05 14:15:11 +0900

    Import https://msbuildlog.com/#api Sample

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Logging.StructuredLogger;
+
+class BinLogReader
+{
+    static void Main(string[] args)
+    {
+        string binLogFilePath = @"C:\temp\test.binlog";
+
+        var binLogReader = new BinLogReader();
+        foreach (var record in binLogReader.ReadRecords(binLogFilePath))
+        {
+            var buildEventArgs = record.Args;
+
+            // print command lines of all tool tasks such as Csc
+            if (buildEventArgs is TaskCommandLineEventArgs taskCommandLine)
+            {
+                Console.WriteLine(taskCommandLine.CommandLine);
+            }
+        }
+    }
+}
